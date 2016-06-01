ab580524da8734441fce956fdb1c7a8c63623561
mariocova3 <mariocova3@gmail.com>
2016-06-01 02:52:46 -0500

    End of Night 6/1.
    Testing bounce component.

---
+++ Assets/destroyObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyObject : MonoBehaviour 
+{
+
+	void OnTriggerEnter(Collider other)
+	{
+		Destroy (other.gameObject);
+	}
+}
---
+++ Assets/moveMerchandise.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveMerchandise : MonoBehaviour 
+{
+
+	public int speed = 3;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		rb.MovePosition (transform.position - (transform.right*speed) * Time.deltaTime);
+	}
+}
